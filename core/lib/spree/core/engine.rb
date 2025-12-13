ae9ea0f430a27e9e5065d80c99694d326c917fe2
Damian Legawiec <damian@getvendo.com>
2025-12-13 09:51:05 +0100

    Fixed: publishing events for products

--- core/lib/spree/core/engine.rb
+++ core/lib/spree/core/engine.rb
@@ -364,6 +364,7 @@ module Spree
           Spree::Post,
           Spree::PostCategory,
           Spree::Price,
+          Spree::Product,
           Spree::Promotion,
           Spree::Refund,
           Spree::ReturnAuthorization,
